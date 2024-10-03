d3be906940aa646cc95e47d2d7b809e979542bc6
Joakim Larsson <joakim.larsson@knowit.se>
2024-10-03 11:06:48 +0200

    Marketing page template added

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
+import MarketingPage from "./MarketingPage";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <MarketingPage />
+  </React.StrictMode>
 );
