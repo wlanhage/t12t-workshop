e67a1c05bc304ebe2a42b0f769150bd273e61dc3
Joakim Larsson <joakim.larsson@knowit.se>
2024-10-06 22:16:53 +0200

    Workshop setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MarketingPage from "./MarketingPage";
+import MarketingPageBrokenKeyboard from "../workshop-manual-testing/01-keyboard-navigation/MarketingPageBrokenKeyboard";
+import MarketingPageBrowser from "../workshop-manual-testing/02-browser-builtin-tools/MarketingPageBrowser";
+import MarketingPagePlugins from "../workshop-manual-testing/03-browser-plugins/MarketingPagePlugins";
+import MarketingPageIntegration from "../workshop-automated-testing/02-cypress-integration/MarketingPageIntegration";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MarketingPage />,
+  },
+  {
+    path: "/manual/keyboard",
+    element: <MarketingPageBrokenKeyboard />,
+  },
+  {
+    path: "/manual/browser",
+    element: <MarketingPageBrowser />,
+  },
+  {
+    path: "/manual/plugins",
+    element: <MarketingPagePlugins />,
+  },
+  {
+    path: "/automated/integration",
+    element: <MarketingPageIntegration />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MarketingPage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
